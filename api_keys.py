cdec91a3d1f2fa48a7b1fb9cea98d3d10b423ce7
Paola Lopez <paola.lopez1@yahoo.com>
2020-11-19 18:13:37 -0800

    Update file

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "cde7ee16f6c576739110d13c5d553492"
+weather_api_key = "c46c4e27b418fe6e21bb06f8c596bfe3"
 
 # Google API Key
 g_key = "AIzaSyCGWFXtD1kB3sRN_Jeu7R4YGX1mt8eMHvg"
